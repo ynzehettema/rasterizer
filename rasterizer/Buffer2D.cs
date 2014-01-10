4addc8ffb6f8e97b42af4bc439f7c896cf4999fc
Ynze Hettema <k_leeuw@live.nl>
2014-01-10 12:38:39 +0100

    initial commit

---
+++ rasterizer/Buffer2D.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
+
+using Pencil.Gaming.MathUtils;
+
+namespace rasterizer
+{
+    public class Buffer2D<T>
+    {
+        public readonly int Width;
+        public readonly int Height;
+        public T[] Data { get; private set; }
+
+        public void Set(int x, int y, T value)
+        {
+            x = Math.Max(0, x);
+            y = Math.Max(0, y);
+            x = Math.Min(Width - 1, x);
+            y = Math.Min(Height - 1, y);
+            Data[y * Width + x] = value;
+        }
+
+        public T Get(int x, int y)
+        {
+            x = Math.Max(x, 0);
+            x = Math.Min(x, Width - 1);
+            y = Math.Max(y, 0);
+            y = Math.Min(y, Height - 1);
+            return Data[y * Width + x];
+        }
+
+        public void Fill(Func<int, int, T> fillFunc)
+        {
+            for (int x = 0; x < Width; ++x)
+            {
+                for (int y = 0; y < Height; ++y)
+                {
+                    Set(x, y, fillFunc(x, y));
+                }
+            }
+        }
+
+        public void Blit(Rectanglei source, Vector2i destPos, Buffer2D<T> dest)
+        {
+            Blit(source, destPos, 1, dest);
+        }
+        public void Blit(Rectanglei source, Vector2i destPos, int scale, Buffer2D<T> dest)
+        {
+            for (int x = source.Left; x < source.Right + source.Width * (scale - 1); ++x)
+            {
+                if (x >= 0 && x < dest.Width)
+                {
+                    for (int y = source.Top; y < source.Bottom + source.Height * (scale - 1); ++y)
+                    {
+                        if (y >= 0 && y < dest.Height)
+                        {
+                            if (typeof(T) != typeof(uint))
+                            {
+                                dest.Set(x - source.Left + destPos.X, y - source.Top + destPos.Y, Get(x / scale, y / scale));
+                            }
+                            else
+                            {
+                                uint color = (uint)((object)Get(x / scale, y / scale));
+                                if ((color & 0xFF000000) != 0x00)
+                                {
+                                    dest.Set(x - source.Left + destPos.X, y - source.Top + destPos.Y, (T)(object)color);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public Buffer2D(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Data = new T[Width * Height];
+        }
+    }
+}
---
+++ rasterizer/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pencil.Gaming;
+using Pencil.Gaming.Graphics;
+using Pencil.Gaming.MathUtils;
+
+using Texture2D = rasterizer.Buffer2D<uint>;
+using Color = System.UInt32;
+
+namespace rasterizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!Glfw.Init())
+            {
+                Console.Error.WriteLine("Failed to initialize GLFW");
+                Environment.Exit(0);
+            }
+            MainGameState main = new MainGameState();
+            main.init();
+            main.run();
+
+        }
+    }
+
+    public class MainGameState
+    {
+        public const int WindowWidth = 800, WindowHeight = 600;
+
+        public const int scale = 1;
+        public const int ScreenWidth = WindowWidth / scale, ScreenHeight = WindowHeight / scale;
+
+        public static Texture2D Screen;
+        private int oglTexture;
+
+        public static Color GetColor(float a, float r, float g, float b)
+        {
+            byte rb = (byte)(a * 255);
+            byte gb = (byte)(r * 255);
+            byte bb = (byte)(g * 255);
+            byte ab = (byte)(b * 255);
+            return (Color)((rb << 24) | (gb << 16) | (bb << 8) | ab);
+
+        }
+
+        public void init()
+        {
+            Glfw.OpenWindowHint(OpenWindowHint.NoResize, 1);
+            Glfw.OpenWindow(WindowWidth, WindowHeight, 8, 8, 8, 0, 24, 0, WindowMode.Window);
+            Glfw.SetWindowPos(100, 100);
+            Glfw.SetWindowTitle("Rasterizer");
+            Glfw.SwapInterval(false);
+            GL.Enable(EnableCap.TextureRectangle);
+            GL.GenTextures(1, out oglTexture);
+            GL.BindTexture(TextureTarget.TextureRectangle, oglTexture);
+            GL.TexParameter(TextureTarget.TextureRectangle, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
+
+            GL.Viewport(0, 0, WindowWidth, WindowHeight);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.Ortho(0.0, WindowWidth, WindowHeight, 0.0, 0.0, 1.0);
+            GL.MatrixMode(MatrixMode.Modelview);
+
+            Screen = new Texture2D(ScreenWidth, ScreenHeight);
+        }
+
+        public void run()
+        {
+            while (Glfw.GetWindowParam(WindowParam.Opened) == 1)
+            {
+                Draw();
+
+                GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+                GL.MatrixMode(MatrixMode.Modelview);
+                GL.LoadIdentity();
+
+
+                GL.Enable(EnableCap.Texture2D);
+
+                GL.BindTexture(TextureTarget.TextureRectangle, oglTexture);
+                GL.TexImage2D(TextureTarget.TextureRectangle, 0, PixelInternalFormat.Rgba, ScreenWidth, ScreenHeight, 0, PixelFormat.Bgra, PixelType.UnsignedByte, Screen.Data);
+
+
+                GL.Begin(BeginMode.Quads);
+
+                GL.TexCoord2(0, 0);
+                GL.Vertex2(0, 0);
+                GL.TexCoord2(ScreenWidth, 0);
+                GL.Vertex2(WindowWidth, 0);
+                GL.TexCoord2(ScreenWidth, ScreenHeight);
+                GL.Vertex2(WindowWidth, WindowHeight);
+                GL.TexCoord2(0, ScreenHeight);
+                GL.Vertex2(0, WindowHeight);
+
+                GL.End();
+
+                Glfw.SwapBuffers();
+                Glfw.PollEvents();
+            }
+        }
+
+        public void Draw()
+        {
+            Screen.Fill((x, y) =>
+            {
+                float yDist = Math.Abs(ScreenHeight / 2 - y);
+                yDist /= ScreenHeight / 2;
+
+                // yDist: 0; z: inf
+                // yDist: 1, z: 1
+                //
+                // z(yDist) = 1 / yDist
+                //
+                // yDist = 1 / z
+                // yDist = | ScreenHeight / 2 - y |;
+                // | ScreenHeight / 2 - y | = 1 / z
+                //
+
+                float zMS = 1f / yDist;
+
+                float xDist = ScreenWidth / 2 - x;
+                xDist /= ScreenWidth / 2;
+
+
+                float xMS = xDist * zMS / 2;
+
+
+
+                zMS += (float)Glfw.GetTime();
+
+                float zCol = tcmod(zMS, 1);
+                float xCol = tcmod(xMS, 1);
+                if (zMS > 3 && zMS < 5 && Math.Abs(xMS) > 1f)
+                {
+                    return GetColor(1, 0.5f, 0.5f, 0.5f);
+                }
+                else
+                {
+                    return GetColor(1, zCol * yDist, xCol * yDist, 0);
+                }
+            });
+
+        }
+
+        private static float tcmod(float x, float mod)
+        {
+            return x < 0 ? Math.Abs(mod - (Math.Abs(x) % mod)) : x % mod;
+        }
+    }
+}
